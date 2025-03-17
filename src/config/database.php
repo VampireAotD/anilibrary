c9c76033a2c15160743d0541e2f57afbb4fee461
Ilya Stepenko <38462532+VampireAotD@users.noreply.github.com>
2025-03-17 16:03:59 +0200

    Added Sentry

--- src/config/database.php
+++ src/config/database.php
@@ -147,8 +147,9 @@ return [
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix'  => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'anilibrary'), '_') . '_database_'),
+            'cluster'    => env('REDIS_CLUSTER', 'redis'),
+            'prefix'     => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'anilibrary'), '_') . '_database_'),
+            'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
         'default' => [
